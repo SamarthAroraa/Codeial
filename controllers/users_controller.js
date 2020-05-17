ce6276f2d42e5d77afca617edc36cdc7d9495b18
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-05-17 23:30:25 +0530

    static sile functionality and layouts+  partials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
-module.exports.profile = function(req,res){
-    res.end('<h1>User Profile </h1>');
-}
+module.exports.profile = function (req, res) {
+  res.render("user_profile", {
+    title: "Your Profile",
+  });
+};
