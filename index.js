c9ec82663e359c957d6e948baf9ac73bce37206d
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-05-19 23:48:11 +0530

    setup static files access for layout

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const expressLayouts = require("express-ejs-layouts");
 
 app.use(express.static("./assets"));
 app.use(expressLayouts);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 app.use("/", require("./routes"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
