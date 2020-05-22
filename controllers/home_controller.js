892b05a3b38a49d91c00548d8a03f9e7fddb3a86
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-05-22 23:18:57 +0530

    sign-up

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,6 @@
 module.exports.home = function (req, res) {
+  console.log(req.cookies);
+  res.cookie("user-id", 25);
   res.render("home", {
     title: " Codial Home",
   });
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,48 @@
+const User = require("../models/user");
 module.exports.profile = function (req, res) {
   res.render("user_profile", {
     title: "Your Profile",
   });
+};
+//render the sign up page
+module.exports.signUp = function (req, res) {
+  res.render("user_sign_up", {
+    title: "Codeial|Sign-up",
+  });
+};
+//render the sign in page
+module.exports.signIn = function (req, res) {
+  res.render("user_sign_in", {
+    title: "Codeial|Sign-in",
+  });
+};
+
+//create a new user
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    console.log("1");
+    return res.redirect("back");
+  }
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user signing user in signing up.");
+      return;
+    }
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating user in database");
+          return;
+        }
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+//sign in and create a session for he user
+module.exports.createSession = function (req, res) {
+  //todo later
 };
